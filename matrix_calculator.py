9df6d2f3a641fb3235e9e4a8557669790be6a98c
Dang Quang <79730898+Rus268@users.noreply.github.com>
2023-11-06 13:36:24 +0000

    Mon, Nov 6, 2023, 8:36 PM +07:00

---
+++ matrix_calculator.py
@@ -0,0 +1,4 @@
+# Ask the user for the first matrix and store it in a dict
+# Ask the user for the secone matrix and store it in the second dict
+# Use matrix formular to calculate the matrix
+# Print the matrix one line at a time.
